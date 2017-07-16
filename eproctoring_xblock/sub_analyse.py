9ad64f4cb7a8c689d42595467961df91020576c9
Pranav Dixit <prnvdixit@gmail.com>
2017-07-16 22:11:15 +0530

    Added Docstring - Removed redundant variables

--- eproctoring_xblock/sub_analyse.py
+++ eproctoring_xblock/sub_analyse.py
@@ -3,16 +3,11 @@ import logging
 import sys
 import subprocess
 
-#def sub_process(path, student_name, course_name, block_id) :
 def sub_process(path, student_name, course_name, block_id) :
-	log = logging.getLogger(__name__)
-	#subprocess.call(["Python", script1, 'arg1', 'arg2'])
-	#command = ['sudo', 'python', '/edx/app/edxapp/venvs/proctor/lib/python2.7/site-packages/suspicious_images/eyeGaze.py', image_data]
-	#command = ['sudo', 'python', '/edx/app/edxapp/venvs/proctor/lib/python2.7/site-packages/suspicious_images/eyeGaze.py', path, student_name, course_name, block_id, image_string]
-	log.info(path)
-	log.info(student_name)
-	log.info(course_name)
-	log.info(block_id)
+	
+	"""
+	Calls Image-Analysing python file as a sub-process.
+	"""
+	
 	command = ['python', '../lib/python2.7/site-packages/eyeGaze.py', path, student_name, course_name, block_id]
-	log.info("YYYYYYYYYYYY")
 	process_call = subprocess.call(command)
