fb96975c5b71503bdfa34f1efcc5f27ac5889b65
Pranav Dixit <prnvdixit@gmail.com>
2017-07-16 22:03:23 +0530

    Added Docstrings to all functions' definitions

--- eproctoring_xblock/database.py
+++ eproctoring_xblock/database.py
@@ -1,13 +1,26 @@
 import MySQLdb
 
+"""
+
+The table is as follows :-
+
+Table Name - suspicious_count
+
+username	course_name	img_count	web_count	tot_count
+---------	---------	---------	---------	---------
+
+"""
+
 def get_user(user_id) :
+    # Set the user, database and password for the database
     sql_user = 'root'
     database = 'edxapp'
     sql_passwd = ""
 
 
     try:
-        db_mysql = MySQLdb.connect(user=sql_user,passwd=sql_passwd, db=database) # Establishing MySQL connection
+	# Establishing MySQL connection
+        db_mysql = MySQLdb.connect(user=sql_user,passwd=sql_passwd, db=database)
     except:
         print "MySQL connection not established"
 
@@ -15,6 +28,8 @@ def get_user(user_id) :
     mysql_cursor = db_mysql.cursor()
     mysql_cursor.execute(query, (user_id,))
     user_name = mysql_cursor.fetchall()
+	
+    # user_name would be of form [(USER_NAME,), (STUDENT_NAME)....]
     return user_name[0][0]
 
 def is_present(username, course_name, db_mysql) :
@@ -26,10 +41,18 @@ def is_present(username, course_name, db_mysql) :
     
 
 def update_table(user_id, course_name, column_name, value) :
+	
+    """
+    This function is called whenever ay suspicious attempt is noticed
+    - Both Image-Analysis and Web-Restriction.
+    """
+
+    # Set the user, database and password for the database
     sql_user = 'root'
     database = 'edxapp'
     sql_passwd = ""
 
+    # Get the unique username from the passed user_id
     username = get_user(user_id)
 
     try:
@@ -37,8 +60,10 @@ def update_table(user_id, course_name, column_name, value) :
     except:
         print "MySQL connection not established"
 
+    # present_bool tells if the given username is present in the given course
     present_bool = is_present(username, course_name, db_mysql)
 
+    # If the (username, course_name) is present in the table, update the corresponding suspicious values
     if present_bool :
 	if column_name == "img_count" :
         	update_query = "update suspicious_count set img_count = %s where username=%s and course_name=%s"
@@ -55,6 +80,7 @@ def update_table(user_id, course_name, column_name, value) :
 
 	db_mysql.commit()
 
+    # Else, insert a new row for given username in given course_name
     else :
 	insert_query = "insert into suspicious_count (username, course_name, web_count, img_count, tot_count) values (%s, %s, %s, %s, %s)"
 	mysql_cursor = db_mysql.cursor()
@@ -70,6 +96,13 @@ def update_table(user_id, course_name, column_name, value) :
 
         
 def final_result(course_name) :
+	
+    """
+    This function is called whenever teacher wants to have an updated order of students
+    with the more suspicious exam attempts of usernames at the top.
+    """
+	
+    # Set the user, database and password for the database
     sql_user = 'root'
     database = 'edxapp'
     sql_passwd = ""
@@ -94,6 +127,13 @@ def final_result(course_name) :
 	
 
 def update_total() :
+	
+	"""
+	This function is called by teacher (by clicking appropriate button in Student view in admin mode)
+	to have tot_count column get updated by all types of suspicious attempts.
+	This function-call shuld precede final_result function-call always.
+	"""
+	
 	sql_user = 'root'
    	database = 'edxapp'
     	sql_passwd = ""
@@ -111,6 +151,12 @@ def update_total() :
 	db_mysql.commit()
 
 def reset_table() :
+	
+	"""
+	This function is called when either starting a new exam OR
+	when teacher is finished analysing the suspicious data.
+	"""
+	
 	sql_user = 'root'
         database = 'edxapp'
         sql_passwd = ""
